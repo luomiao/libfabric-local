ed84862dbc7d2b561788c54ad41ec0c9b78554ae
Jithin Jose <jithin.jose@intel.com>
2014-12-18 11:43:06 -0800

    Fix return type of av-insert

--- prov/sockets/src/sock_av.c
+++ prov/sockets/src/sock_av.c
@@ -73,7 +73,6 @@ fi_addr_t sock_av_lookup_key(struct sock_av *av, int key)
 	return FI_ADDR_NOTAVAIL;
 }
 
-
 struct sock_conn *sock_av_lookup_addr(struct sock_av *av, 
 		fi_addr_t addr)
 {
@@ -94,9 +93,8 @@ struct sock_conn *sock_av_lookup_addr(struct sock_av *av,
 
 	av_addr = idm_lookup(&av->addr_idm, index);
 	if (!av_addr->key) {
-		av_addr->key = sock_conn_map_match_or_connect(
-			av->cmap, 
-			(struct sockaddr_in*)&av_addr->addr, 0);
+		av_addr->key = sock_conn_map_match_or_connect(av->cmap, 
+				(struct sockaddr_in*)&av_addr->addr, 0);
 		if (!av_addr->key) {
 			SOCK_LOG_ERROR("failed to match or connect to addr %lu\n", addr);
 			errno = EINVAL;
@@ -107,31 +105,34 @@ struct sock_conn *sock_av_lookup_addr(struct sock_av *av,
 }
 
 static int sock_check_table_in(struct sock_av *_av, struct sockaddr_in *addr,
-		int count)
+			       fi_addr_t *fi_addr, int count)
 {
-	int i;
+	int i, ret;
 	struct sock_av_addr *av_addr;
 	av_addr = calloc(count, sizeof(struct sock_av_addr));
 	if (!av_addr)
 		return -ENOMEM;
-
-	for (i=0; i<count; i++) {
+	
+	for (i=0, ret = 0; i<count; i++) {
 		memcpy(&av_addr[i].addr, &addr[i], sizeof(struct sockaddr_in));
-		if (idm_set(&_av->addr_idm, _av->stored, &av_addr[i]) < 0)
-			goto err;
+		if (idm_set(&_av->addr_idm, _av->stored, &av_addr[i]) < 0) {
+			if (fi_addr)
+				fi_addr[i] = FI_ADDR_NOTAVAIL;
+			continue;
+		}
+
+		if (fi_addr)
+			fi_addr[i] = (fi_addr_t)i;
 
 		_av->stored++;
+		ret++;
 	}
-
-	return 0;
-
-err:
-	free(av_addr);
-	return -errno;
+	
+	return ret;
 }
 
-static int sock_at_insert(struct fid_av *av, const void *addr, size_t count,
-			  fi_addr_t *fi_addr, uint64_t flagsi, void *context)
+static int sock_av_insert(struct fid_av *av, const void *addr, size_t count,
+			  fi_addr_t *fi_addr, uint64_t flags, void *context)
 {
 	struct sock_av *_av;
 
@@ -139,7 +140,8 @@ static int sock_at_insert(struct fid_av *av, const void *addr, size_t count,
 
 	switch(((struct sockaddr *)addr)->sa_family) {
 	case AF_INET:
-		return sock_check_table_in(_av, (struct sockaddr_in *)addr, count);
+		return sock_check_table_in(_av, (struct sockaddr_in *)addr, 
+					   fi_addr, count);
 	default:
 		SOCK_LOG_ERROR("invalid address type inserted: only IPv4 supported\n");
 		return -EINVAL;
@@ -178,17 +180,6 @@ static const char * sock_at_straddr(struct fid_av *av, const void *addr,
 	return NULL;
 }
 
-static int sock_am_insert(struct fid_av *av, const void *addr, size_t count,
-			  fi_addr_t *fi_addr, uint64_t flags, void *context)
-{
-	int i;
-	sock_at_insert(av, addr, count, fi_addr, flags, context);
-	for (i = 0; i < count; i++)
-		fi_addr[i] = (fi_addr_t)i;
-
-	return 0;
-}
-
 int sock_insertsvc(struct fid_av *av, const char *node,
 		   const char *service, fi_addr_t *fi_addr,
 		   uint64_t flags, void *context)
@@ -205,14 +196,7 @@ int sock_insertsvc(struct fid_av *av, const char *node,
 	if (!ret)
 		return -FI_EINVAL;
 
-	if (_av->attr.type == FI_AV_MAP) {
-		ret = sock_am_insert(av, result->ai_addr, 1, 
-				     fi_addr, flags, context);
-	} else {
-		ret = sock_at_insert(av, result->ai_addr, 1, 
-				     fi_addr, flags, context);
-	}
-
+	ret = sock_av_insert(av, result->ai_addr, 1, fi_addr, flags, context);
 	freeaddrinfo(result); 
 	return ret;
 }
@@ -281,7 +265,7 @@ static struct fi_ops sock_av_fi_ops = {
 
 static struct fi_ops_av sock_am_ops = {
 	.size = sizeof(struct fi_ops_av),
-	.insert = sock_am_insert,
+	.insert = sock_av_insert,
 	.insertsvc = sock_insertsvc,
 	.remove = sock_am_remove,
 	.lookup = sock_am_lookup,
@@ -290,7 +274,7 @@ static struct fi_ops_av sock_am_ops = {
 
 static struct fi_ops_av sock_at_ops = {
 	.size = sizeof(struct fi_ops_av),
-	.insert = sock_at_insert,
+	.insert = sock_av_insert,
 	.insertsvc = sock_insertsvc,
 	.remove = sock_at_remove,
 	.lookup = sock_at_lookup,
